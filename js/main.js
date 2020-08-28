ff7b518d0d6cd7bc878c6f025657d91a7d31d81f
RAKSHITH RAMESH <56740995+rakbarca10@users.noreply.github.com>
2020-08-28 14:48:37 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -64,3 +64,18 @@ $(document).ready(function () {
     navbarFixed();
 
 });
+
+
+
+
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:1,
+        itemsDesktop:[1000,1],
+        itemsDesktopSmall:[979,1],
+        itemsTablet:[769,1],
+        pagination:true,
+        transitionStyle:"goDown",
+        autoplay:true
+    });
+});
