bb0650934c49b1a1a3434556327795b09711f270
RAKSHITH RAMESH <56740995+rakbarca10@users.noreply.github.com>
2021-06-17 16:13:13 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -70,14 +70,9 @@ $(document).ready(function () {
 
 
 // Testimonial
-$(document).ready(function(){
-    $("#testimonial-slider").owlCarousel({
-        items:1,
-        itemsDesktop:[1000,1],
-        itemsDesktopSmall:[979,1],
-        itemsTablet:[769,1],
-        pagination:true,
-        transitionStyle:"goDown",
-        autoplay:true
-    });
-});
+ $(".testimonials-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
